4194165bb49affaa3441fdb5dc0524ef3daed7a1
Ivan Alonso <ivanalons@gmail.com>
2020-07-22 11:40:21 +0200

    Generate Empty API Structure

---
+++ SoccerProject/src/main/java/com/itacademy/soccer/SoccerProjectApplication.java
@@ -0,0 +1,13 @@
+package com.itacademy.soccer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoccerProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoccerProjectApplication.class, args);
+	}
+
+}
---
+++ SoccerProject/src/test/java/com/itacademy/soccer/SoccerProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itacademy.soccer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoccerProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
