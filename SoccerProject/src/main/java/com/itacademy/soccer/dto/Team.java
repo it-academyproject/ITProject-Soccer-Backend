f2dedd653557db4d369307a68a69d5dace0c0c6f
kevin <kevin.haesendonck@gmail.com>
2020-07-28 10:18:57 +0200

    added dto and dao Teams

---
+++ SoccerProject/src/main/java/com/itacademy/soccer/dto/Team.java
@@ -0,0 +1,152 @@
+/**
+ * @author KevHaes
+ *
+ */
+package com.itacademy.soccer.dto;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * @author KevHaes
+ *
+ */
+@Entity
+public class Team {
+	/////////////// ATRIBUTES ///////////////
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private Date foundation_date;
+	private String badge;
+	private Double budget;
+	private int wins;
+	private int losses;
+	private int draws;
+
+	/////////////// CONSTRUCTORS ///////////////
+	public Team(Long id, Date foundation_date, String badge, Double budget, int wins, int losses, int draws) {
+		this.id = id;
+		this.foundation_date = foundation_date;
+		this.badge = badge;
+		this.budget = budget;
+		this.wins = wins;
+		this.losses = losses;
+		this.draws = draws;
+	}
+
+	public Team() {
+	}
+
+	/////////////// GETTERS & SETTERS ///////////////
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the foundation_date
+	 */
+	public Date getFoundation_date() {
+		return foundation_date;
+	}
+
+	/**
+	 * @param foundation_date the foundation_date to set
+	 */
+	public void setFoundation_date(Date foundation_date) {
+		this.foundation_date = foundation_date;
+	}
+
+	/**
+	 * @return the badge
+	 */
+	public String getBadge() {
+		return badge;
+	}
+
+	/**
+	 * @param badge the badge to set
+	 */
+	public void setBadge(String badge) {
+		this.badge = badge;
+	}
+
+	/**
+	 * @return the budget
+	 */
+	public Double getBudget() {
+		return budget;
+	}
+
+	/**
+	 * @param budget the budget to set
+	 */
+	public void setBudget(Double budget) {
+		this.budget = budget;
+	}
+
+	/**
+	 * @return the wins
+	 */
+	public int getWins() {
+		return wins;
+	}
+
+	/**
+	 * @param wins the wins to set
+	 */
+	public void setWins(int wins) {
+		this.wins = wins;
+	}
+
+	/**
+	 * @return the losses
+	 */
+	public int getLosses() {
+		return losses;
+	}
+
+	/**
+	 * @param losses the losses to set
+	 */
+	public void setLosses(int losses) {
+		this.losses = losses;
+	}
+
+	/**
+	 * @return the draws
+	 */
+	public int getDraws() {
+		return draws;
+	}
+
+	/**
+	 * @param draws the draws to set
+	 */
+	public void setDraws(int draws) {
+		this.draws = draws;
+	}
+
+	/////////////// TOSTRING ///////////////
+
+	@Override
+	public String toString() {
+		return "Team [id=" + id + ", foundation_date=" + foundation_date + ", badge=" + badge + ", budget=" + budget
+				+ ", wins=" + wins + ", losses=" + losses + ", draws=" + draws + "]";
+	}
+
+}
