b7ff9c20c626c9c8966e876dca1d49c10e7ce6df
Daniel Fernandez <dfzfz7@gmail.com>
2020-10-20 11:56:28 +0200

    typeUser field in JSON response changed to type_user

--- SoccerProject/src/main/java/com/itacademy/soccer/dto/User.java
+++ SoccerProject/src/main/java/com/itacademy/soccer/dto/User.java
@@ -1,6 +1,7 @@
 package com.itacademy.soccer.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.itacademy.soccer.dto.typeUser.TypeUser;
 
 import javax.persistence.*;
@@ -15,8 +16,9 @@ public class User
     private Long id; // Id User Auto-Generated
 
     @Column(name="type")
+    @JsonProperty("type_user") // Change the field name to "type_user" in JSON Response 
     @Enumerated(EnumType.STRING) // IMPORTANT: This converts 0 or 1 to Manager or Admin (INTEGER TO STRING)
-    private TypeUser type_user;
+    private TypeUser typeUser;
 
     @Column(name="email")
     private String email; // User Email = Name User
@@ -45,12 +47,12 @@ public class User
 
     public TypeUser getTypeUser()
     {
-        return type_user;
+        return typeUser;
     }
 
-    public void setTypeUser(TypeUser type_user)
+    public void setTypeUser(TypeUser typeUser)
     {
-        this.type_user = type_user;
+        this.typeUser = typeUser;
     }
 
     public String getEmail()
