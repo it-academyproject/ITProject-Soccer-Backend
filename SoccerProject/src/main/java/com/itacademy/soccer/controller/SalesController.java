8211c61e8581cd2d99fa1a0215de393f0d0b0762
Fauno Guazina <prof.guazina@gmail.com>
2020-11-20 10:38:45 +0100

    updating POST and PUT methods in Sale
    - accidentally correcting the spacing, SORRY!
    - POST: 1st check if a player exists, then if he does not have an active sale, afterwards if the price is above zero, lastly if the date is at least 3 days after posting.
    - PUT: 1st verifies if the sale exists, then if there is a date update if it is non-null, equal to the existing one or previous to it, then verifies that the price is not null, negative or equal to the current one. if there is neither price nor date does not proceed with change

--- SoccerProject/src/main/java/com/itacademy/soccer/controller/SalesController.java
+++ SoccerProject/src/main/java/com/itacademy/soccer/controller/SalesController.java
@@ -1,11 +1,16 @@
 package com.itacademy.soccer.controller;
 
-import java.util.*;
-
-import com.itacademy.soccer.dao.IPlayerDAO;
 import com.itacademy.soccer.dto.Player;
 import com.itacademy.soccer.service.impl.BidServiceImpl;
 import com.itacademy.soccer.service.impl.PlayerServiceImpl;
+
+import java.util.Comparator;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -22,6 +27,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.itacademy.soccer.controller.json.SaleJson;
 import com.itacademy.soccer.dto.Sale;
 import com.itacademy.soccer.service.impl.SaleServiceImpl;
+import com.itacademy.soccer.util.Verify;
 
 @RestController
 @PreAuthorize("authenticated")
@@ -31,35 +37,31 @@ public class SalesController {
 	@Autowired
 	SaleServiceImpl saleServiceImpl;
 
-	//B26
 	@Autowired
 	PlayerServiceImpl playerServiceImpl;
 
-	//B29
 	@Autowired
 	BidServiceImpl bidServiceImpl;
 
-	IPlayerDAO iPlayerDAO;
-
 	@GetMapping("/sales")
-	public HashMap<String,Object> listAllSales(){
-		
+	public HashMap<String, Object> listAllSales() {
+
 		List<Sale> salesList = saleServiceImpl.listAllSales();
 		List<SaleJson> jsonList = SaleJson.parseListSaleToJson(salesList);
-		
-		HashMap<String,Object> map = new HashMap<>();
+
+		HashMap<String, Object> map = new HashMap<>();
 		map.put("sales", jsonList);
 		map.put("success", true);
 		map.put("message", "All working perfectly");
-		
+
 		return map;
 	}
 
 	@GetMapping("/sales/{id}")
-	public HashMap<String,Object> getSalesById(@PathVariable(name="id") Long salesId){
+	public HashMap<String, Object> getSalesById(@PathVariable(name = "id") Long salesId) {
 
-		HashMap<String,Object> map = new HashMap<>();
-		
+		HashMap<String, Object> map = new HashMap<>();
+
 		try {
 			Sale sale = saleServiceImpl.getSaleById(salesId);
 			SaleJson json = SaleJson.parseSaleToJson(sale);
@@ -67,116 +69,164 @@ public class SalesController {
 			map.put("sale", json);
 			map.put("success", true);
 			map.put("message", "All working perfectly");
-			
-		} catch( NoSuchElementException e) {
+
+		} catch (NoSuchElementException e) {
 			map.put("sale", null);
 			map.put("success", false);
-			map.put("message", "Sale with id "+salesId+" not found.");
+			map.put("message", "Sale with id " + salesId + " not found.");
 		}
-		
-		
+
 		return map;
 	}
-	
+
 	@GetMapping("/sales/players/{id}")
-	//B26
-	HashMap<String,Object> getSalesByPlayer(@PathVariable Long id){
-		HashMap<String,Object> map = new HashMap<>();
+	// B26
+	HashMap<String, Object> getSalesByPlayer(@PathVariable Long id) {
+		HashMap<String, Object> map = new HashMap<>();
 		try {
-			Optional<Player> myplayer = iPlayerDAO.findById(id);
+			Optional<Player> myplayer = playerServiceImpl.findById(id);
 			List<Sale> allSalesByPlayer = saleServiceImpl.saleListByPlayer(myplayer.get());
 
-			if(allSalesByPlayer.size() > 0) {
+			if (allSalesByPlayer.size() > 0) {
 				map.put("success", true);
 				map.put("all sales by player id", allSalesByPlayer);
 				map.put("message", "get all sales by player id");
-			}else {
+			} else {
 				map.put("success", false);
 				map.put("message", "Error getting all sales");
 
-				//throw new Exception();
+				// throw new Exception();
 			}
 
-		}
-		catch (Exception e) {
+		} catch (Exception e) {
 			map.put("success", false);
 			map.put("message", "something went wrong: " + e.getMessage());
 		}
 		return map;
 	}
-	
 
 	@GetMapping("/sales/filter")
-	HashMap<String,Object> getFilteredSales(
-			@RequestParam(required=false, value="maxAge", defaultValue="100") int maxage, 
-			@RequestParam(required=false, value="minAge", defaultValue="1") int minage,
-			@RequestParam(required=false, value="defense", defaultValue="1") int defense, 
-			@RequestParam(required=false, value="attack", defaultValue="1") int attack,
-			@RequestParam(required=false, value="keeper", defaultValue="1") int keeper,
-			@RequestParam(required=false, value="pass", defaultValue="1") int pass) {
-		return saleServiceImpl.salesFilter(maxage, minage, defense, attack, keeper, pass);
+	HashMap<String, Object> getFilteredSales(
+			@RequestParam(required = false, value = "maxAge", defaultValue = "100") int ageMax,
+			@RequestParam(required = false, value = "minAge", defaultValue = "1") int ageMin,
+			@RequestParam(required = false, value = "defense", defaultValue = "1") int defense,
+			@RequestParam(required = false, value = "attack", defaultValue = "1") int attack,
+			@RequestParam(required = false, value = "keeper", defaultValue = "1") int keeper,
+			@RequestParam(required = false, value = "pass", defaultValue = "1") int pass) {
+		return saleServiceImpl.salesFilter(ageMax, ageMin, defense, attack, keeper, pass);
 	}
-	
-	
-	@PostMapping("/sales") //@RequestBody Sale p_sale
-	public HashMap<String,Object> createSale(@RequestBody SaleJson saleJson){
-		
-		Sale pSale = saleJson.toSale();
-		
-		Sale sale = saleServiceImpl.createSale(pSale);
-		SaleJson json = SaleJson.parseSaleToJson(sale);
-		
-		HashMap<String,Object> map = new HashMap<>();
-		map.put("sale", json);
-		map.put("success", true);
-		map.put("message", "Sale created perfectly");
-		
-		return map;
-	}
-	
-	@PutMapping("/sales") 
-	public HashMap<String,Object> updateSale(@RequestBody SaleJson saleJson){
-		
-		Sale pSale = saleJson.toSale();
-		
-		Sale updatedSale = null;
-		HashMap<String,Object> map = new HashMap<>();
-		map.put("saleId", saleJson.getId());
+
+	@PostMapping("/sales")
+	public HashMap<String, Object> createSale(@RequestParam Long playerId, @RequestBody Sale sale) {
+
+		HashMap<String, Object> map = new HashMap<>();
 
 		try {
-			updatedSale = saleServiceImpl.updateSale(saleJson.getId(), pSale);
-			SaleJson json = SaleJson.parseSaleToJson(updatedSale);
+			Optional<Player> player = playerServiceImpl.findById(playerId);
+			String message = "Sale created perfectly";
+
+			if (!player.isPresent()) {
+				throw new Exception("Player ID doesn't exist!");
+
+			} else {
+				Optional<Sale> last = saleServiceImpl.saleListByPlayer(player.get()).stream()
+						.sorted(Comparator.comparing(Sale::getLimitDate).reversed()).findFirst();
+				
+				if (last.isPresent() && !Verify.isExpired(last.get().getLimitDate())) {
+					throw new Exception("That player already has a sale in progress!");	
+				}
+			}
 			
-			map.put("sale", json);
+			if (!Verify.notNullZero(sale.getInitialPrice())) {
+				throw new Exception("Initial Price is null or empty");
+
+			} else if (!Verify.notNullAfter3Days(sale.getLimitDate())) {
+				sale.setLimitDate(new Date(System.currentTimeMillis() + 259_080_000));
+				message = "Sale created with date change, as the minimum is 3 days";
+			}
+			
+			Sale newSale = new Sale();
+			newSale.setInitialPrice(sale.getInitialPrice());
+			newSale.setLimitDate(sale.getLimitDate());
+			newSale.setPlayer(player.get());
+
+			newSale = saleServiceImpl.createSale(newSale);
+
+			map.put("sale", SaleJson.parseSaleToJson(newSale));
 			map.put("success", true);
-			map.put("message", "Sale updated perfectly");
-		}catch(NoSuchElementException e) {
-			map.put("sale", null);
+			map.put("message", message);
+
+		} catch (Exception e) {
 			map.put("success", false);
-			map.put("message", "Sale with id "+saleJson.getId()+" doesn't exist.");
+			map.put("message", "Sale NOT Created : " + e.getMessage());
 		}
-		
+
 		return map;
 	}
 
-	
-	@DeleteMapping("/sales/{id}") 
-	public HashMap<String,Object> deleteSale(@PathVariable(name="id") Long saleId){
-		
-		HashMap<String,Object> map = new HashMap<>();
+	@PutMapping("/sales/{id}")
+	public HashMap<String, Object> updateSale(@PathVariable(name = "id") Long id, @RequestBody Sale sale) {
+
+		HashMap<String, Object> map = new HashMap<>();
+
+		try {
+			Optional<Sale> oldSale = saleServiceImpl.getOptionalSale(id);
+
+			if (!oldSale.isPresent()) {
+				throw new Exception("This Sale ID doesn't exist!");
+				
+			}
+			
+			if (sale.getLimitDate() != null) {
+				
+				if (Verify.notNullEqualsBefore(sale.getLimitDate(), oldSale.get().getLimitDate())) {
+					
+					oldSale.get().setLimitDate(sale.getLimitDate());
+					
+				} else {
+					throw new Exception("Limit Date can't be null, before or same than current!");
+				}
+				
+			}
+			
+			if (Verify.notLessEqZeroEquals(sale.getInitialPrice(), oldSale.get().getInitialPrice())) {
+				oldSale.get().setInitialPrice(sale.getInitialPrice());
+				
+			} else if (sale.getLimitDate() == null) {
+				throw new Exception("The Initial Price can't be null, zero, negative or equal the current value!");
+			}
+
+			Sale newSale = saleServiceImpl.updateSale(oldSale.get());
+
+			map.put("success", true);
+			map.put("sale", SaleJson.parseSaleToJson(newSale));
+			map.put("message", "Sale updated perfectly");
+			
+		} catch (Exception e) {
+			map.put("success", false);
+			map.put("message", "Sale NOT Updated : " + e.getMessage());
+		}
+
+		return map;
+	}
+
+	@DeleteMapping("/sales/{id}")
+	public HashMap<String, Object> deleteSale(@PathVariable(name = "id") Long saleId) {
+
+		HashMap<String, Object> map = new HashMap<>();
 
 		try {
 			saleServiceImpl.deleteSale(saleId);
-			
+
 			map.put("saleId", saleId);
 			map.put("success", true);
 			map.put("message", "Sale deleted perfectly");
-		}catch(EmptyResultDataAccessException e){
+		} catch (EmptyResultDataAccessException e) {
 			map.put("saleId", saleId);
 			map.put("success", false);
-			map.put("message", "Sale with id "+saleId+" doesn't exist.");
+			map.put("message", "Sale with id " + saleId + " doesn't exist.");
 		}
-		
+
 		return map;
 	}
 
