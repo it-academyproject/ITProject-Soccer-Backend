6fe885c5116c03537ae47fdca731f204c2e8662a
Fauno Guazina <prof.guazina@gmail.com>
2020-11-20 10:47:25 +0100

    updating POST and PUT methods in Bid
    - I noticed that there were comments about security and I took the liberty to add them.
    - POST: first check if the sale exists, then if the ime exists, then if the team has a budget, then if the price is not zero or zero or less than the purchase, then check the last bid and if the value is at least X% higher ( this variable is configured in the method), so it checks if the sale is not already expired, so if there is only 2min left to end the term, it extends another 2 min.
    - PUT: you can only change the price and upwards, never downwards, if it is necessary to lower the value, you must delete the bid and proceed with another one.

--- SoccerProject/src/main/java/com/itacademy/soccer/controller/BidsController.java
+++ SoccerProject/src/main/java/com/itacademy/soccer/controller/BidsController.java
@@ -4,11 +4,14 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.Optional;
 
 import com.itacademy.soccer.dto.Sale;
 import com.itacademy.soccer.dto.Team;
 import com.itacademy.soccer.service.impl.SaleServiceImpl;
 import com.itacademy.soccer.service.impl.TeamServiceImpl;
+import com.itacademy.soccer.util.Verify;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -26,7 +29,7 @@ import com.itacademy.soccer.dto.Bid;
 import com.itacademy.soccer.service.impl.BidServiceImpl;
 
 @RestController
-@PreAuthorize("authenticated")
+@PreAuthorize("hasRole('ADMIN') OR hasRole('MANAGER')")
 @RequestMapping("/api")
 public class BidsController {
 
@@ -39,7 +42,7 @@ public class BidsController {
 	@Autowired
 	TeamServiceImpl teamServiceImpl;
 
-	@GetMapping("/sales/{id}/bids") // Accesible desde usuario MANAGER o ADMIN (pendiente)
+	@GetMapping("/sales/{id}/bids")
 	public HashMap<String, Object> listBidsBySale(@PathVariable(name = "id") Long salesId) {
 
 		HashMap<String, Object> map = new HashMap<>();
@@ -61,137 +64,116 @@ public class BidsController {
 		return map;
 	}
 
-	// TODO B29
-	@PostMapping("/bids/sales/{id}")
-	public HashMap<String, Object> createBid(@PathVariable Long id, @RequestBody Bid bid) {
+	@PostMapping("/sales/{id}/bids")
+	public HashMap<String, Object> createBid(@PathVariable(name = "id") Long id, @RequestBody Bid bid) {
 
 		HashMap<String, Object> map = new HashMap<>();
-		Date now = new Date();
-		String msj = "";
 
-		Bid newBid = new Bid();
+		try {
+			Optional<Sale> sale = saleServiceImpl.getOptionalSale(id);
+			Optional<Team> team = teamServiceImpl.getOptionalTeam(bid.getTeamId());
+			Optional<Bid> lastBid = (!sale.isPresent()) ? Optional.empty()
+					: bidServiceImpl.getLastBidBySaleId(sale.get().getId());
+			float percentage = 1.2f; // equivalent to 20% than the last bid
 
-		newBid = bid;
-		System.out.println("id_teamd " + bid.getTeamId());
-		System.out.println("price_bid " + bid.getBidPrice());
+			if (!sale.isPresent()) {
 
-		newBid.setBidPrice(bid.getBidPrice());
+				throw new Exception("The Sale Id is incorrect!");
 
-		Sale sale = saleServiceImpl.getSaleById(id);
+			} else if (!team.isPresent()) {
 
-		// SALE IS CLOSED
-		if (sale.getLimitDate().before(now)) {
+				throw new Exception("The Team Id is incorrect!");
 
-			msj = "Sale " + sale.getId() + " is closed";
-			map.put("message", msj);
-			map.put("Date_limit", sale.getLimitDate());
+			} else if (team.get().getBudget() < bid.getBidPrice()) {
 
-			// SALE IS OPEN
-		} else {
+				throw new Exception("This team does not have enough budget for this bid!");
 
-			newBid.setSale(sale);
+			} else if (!Verify.notNullZero(bid.getBidPrice()) || bid.getBidPrice() < sale.get().getInitialPrice()) {
 
-			Team team = teamServiceImpl.getOneTeamById(newBid.getTeamId());
+				throw new Exception("The Price of Bid can't be null or less than the initial sale!");
 
-			// TEAM BUDGET INFERIOR TO PRICE'S BID
-			if (newBid.getBidPrice() > team.getBudget()) {
+			} else if (lastBid.isPresent()
+					&& !Verify.isPercentHigher(bid.getBidPrice(), lastBid.get().getBidPrice(), percentage)) {
+				throw new Exception(
+						"The Bid Price must be at least " + ((int) (percentage * 100 - 100)) + "% higher than the last bid");
 
-				msj = "Team " + team.getId() + " doesn't have enough budget";
-				map.put("message", msj);
-				map.put("Team_budget", team.getBudget());
-				map.put("Sale_price", sale.getInitialPrice());
+			} else if (Verify.isExpired(sale.get().getLimitDate())) {
 
-			} else {
-				// PRICE'S BID INFERIOR TO PRICE'S SALE
-				if (sale.getInitialPrice() > newBid.getBidPrice()) {
+				throw new Exception("This Sale is already expired!!");
 
-					msj = "BID " + newBid.getBidPrice() + " the offer is less than the sale price";
-					map.put("message", msj);
-					map.put("Team_budget", team.getBudget());
-					map.put("Sale_price", sale.getInitialPrice());
+			} else if (Verify.inTwoMinToExpire(new Date(), sale.get().getLimitDate())) {
 
-				} else {
-					// System.out.println(newBid.getTeam_id());
-					newBid.setTeam(teamServiceImpl.getOneTeamById(newBid.getTeamId()));
-					// Date now = new Date();
-					newBid.setOperationDate(now);
-					try {
-						Bid NewlyCreatedBid = bidServiceImpl.save(newBid);
-						map.put("success", true);
-						map.put("message", "Bid Created");
-						map.put("bid", NewlyCreatedBid);
-					} catch (Exception e) {
-						map.put("success", false);
-						map.put("message", "Bid NOT Created ! :" + e.getMessage());
-					}
-
+				if (sale.get().getFirstLimitDate() == null) {
+					sale.get().setFirstLimitDate(sale.get().getLimitDate());
 				}
+
+				sale.get().setLimitDate(new Date(sale.get().getLimitDate().getTime() + 120000));
+
+				sale = Optional.ofNullable(saleServiceImpl.updateSale(sale.get()));
+
+				if (!sale.isPresent()) {
+					throw new Exception("Something went wrong with updating the sale deadline!");
+				}
+			}
+
+			Bid newBid = new Bid();
+			newBid.setTeamId(team.get().getId());
+			newBid.setBidPrice(bid.getBidPrice());
+			newBid.setOperationDate(new Date());
+			newBid.setSale(sale.get());
+			newBid.setTeam(team.get());
+
+			newBid = bidServiceImpl.createBid(newBid);
+
+			map.put("Bid", BidJson.parseBidToJson(newBid));
+			map.put("success", true);
+			map.put("message", "Bid was created succefull!");
+			
+		} catch (Exception e) {
+			map.put("success", false);
+			map.put("message", "BID isn't created! : " + e.getMessage());
+		}
+
+		return map;
+	}
+
+	@PreAuthorize("hasRole('ADMIN')")
+	@PutMapping("/bids/{id}")
+	public HashMap<String, Object> updateBid(@PathVariable(name = "id") Long id, @RequestBody Bid bid) {
+
+		HashMap<String, Object> map = new HashMap<>();
+
+		try {
+
+			Optional<Bid> oldBid = bidServiceImpl.getBidById(id);
+
+			if (!oldBid.isPresent()) {
+
+				throw new Exception("The Bid Id is incorrect!");
+
+			} else if (!Verify.notNullZero(bid.getBidPrice()) || oldBid.get().getBidPrice() >= bid.getBidPrice()) {
+
+				throw new Exception("The Price of Bid can't be null, zero or higher than the current Bid!");
 
 			}
 
-		}
+			oldBid.get().setBidPrice(bid.getBidPrice());
 
-		return map;
-	}
+			Bid newBid = bidServiceImpl.updateBid(oldBid.get());
 
-	@PostMapping("/sales/{id}/bids") // Accesible desde usuario MANAGER (pendiente)
-	public HashMap<String, Object> createBidBySale(@PathVariable(name = "id") Long salesId,
-			@RequestBody BidJson bidJson) {
-
-		Bid bid = bidJson.toBid(); // convertir json en formato dto
-
-		HashMap<String, Object> map = new HashMap<>();
-
-		Long teamId = bidJson.getTeamId();
-
-		try {
-
-			Bid createdBid = bidServiceImpl.createBidBySale(salesId, bid); // ejecutar service
-			BidJson json = BidJson.parseBidToJson(createdBid); // convertir dto en formato json
-
-			map.put("bid", json);
+			map.put("bid", BidJson.parseBidToJson(newBid));
 			map.put("success", true);
 			map.put("message", "All working perfectly");
-
-		} catch (NoSuchElementException e) { // si sale o team no existe
-
+			
+		} catch (Exception e) {
 			map.put("success", false);
-			map.put("message", "Sale with id " + salesId + " or Team with id " + teamId + " doesn't exist.");
-		}
-
-		return map;
-
-	}
-
-	// SOLO Accesible desde usuario ADMIN (pendiente)
-	// Funcionalidad por parte del Admin para modificar el precio de una puja (bid)
-	// en caso de que
-	// un equipo haya realizado una puja con un precio erroneo
-	// Ejemplo: un equipo hizo una puja por 3300 y se queria hacer una puja de 300
-	// se le comunica al admin y el admin modifica el precio correcto
-	@PutMapping("/bids/{id}")
-	public HashMap<String, Object> updateBid(@PathVariable(name = "id") Long bidId, @RequestBody BidJson jsonBid) {
-		Bid bid = jsonBid.toBid(); // convertir formato json en formato dto
-
-		HashMap<String, Object> map = new HashMap<>();
-
-		try {
-
-			Bid updatedBid = bidServiceImpl.updateBid(bidId, bid); // implementación del service
-			BidJson json = BidJson.parseBidToJson(updatedBid); // convertir formato dto en json
-
-			map.put("bid", json);
-			map.put("success", true);
-			map.put("message", "All working perfectly");
-		} catch (NoSuchElementException e) { // si bid no existe
-			map.put("success", false);
-			map.put("message", "Bid with id " + bidId + "doesn't exist.");
+			map.put("message", "Bid was not updated! : " + e.getMessage());
 		}
 
 		return map;
 	}
 
-	// SOLO Accesible desde usuario ADMIN (pendiente)
+	@PreAuthorize("hasRole('ADMIN')")
 	@DeleteMapping("/bids/{id}")
 	public HashMap<String, Object> deleteBid(@PathVariable(name = "id") Long bidId) {
 
@@ -209,8 +191,7 @@ public class BidsController {
 		return map;
 	}
 
-	// SOLO Accesible desde usuario ADMIN (pendiente)
-	// Elimina la puja más reciente (ultima puja) de una venta concreta
+	@PreAuthorize("hasRole('ADMIN')")
 	@DeleteMapping("/sales/{id}/lastbid")
 	public HashMap<String, Object> deleteLastBidBySale(@PathVariable(name = "id") Long salesId) {
 
